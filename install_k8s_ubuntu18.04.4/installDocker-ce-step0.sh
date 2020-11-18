e677878771ab7f1f9f6cc2039466229ff90fae7f
tom <121294318@qq.com>
2020-11-18 11:19:12 +0800

    Update installDocker-ce-step0.sh

--- install_k8s_ubuntu18.04.4/installDocker-ce-step0.sh
+++ install_k8s_ubuntu18.04.4/installDocker-ce-step0.sh
@@ -1,35 +1,6 @@
-#sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get remove -y docker-*
+curl -sSL https://get.docker.com | sudo sh
 
-sudo apt-get update
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
+# 设置 Docker 开机自启动
+sudo systemctl enable docker
+# 启动 Docker
 sudo systemctl start docker
-
-sudo docker version
-
-cat > /etc/docker/daemon.json <<EOF 
-{
-	"exec-opts": ["native.cgroupdriver=systemd"],	
-	"log-driver": "json-file",
-	"log-opts": {"max-size": "100m" },
-	"storage-driver": "overlay2", "storage-opts": ["overlay2.override_kernel_check=true" ]
-}
-EOF

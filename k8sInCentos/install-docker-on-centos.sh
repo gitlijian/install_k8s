a68f3030d7c68513d09a865753da1b3dc7c58cc6
tom <121294318@qq.com>
2020-11-18 11:18:02 +0800

    Update install-docker-on-centos.sh

--- k8sInCentos/install-docker-on-centos.sh
+++ k8sInCentos/install-docker-on-centos.sh
@@ -1,33 +1,6 @@
-#!/bin/sh
-# https://docs.docker.com/engine/install/centos/
+curl -sSL https://get.docker.com | sudo sh
 
-# 0. 关闭防火墙，清iptables
-sudo setenforce 0
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
-sudo iptables -L
-sudo iptables --flush
-
-# 1. 首先去掉已有安装
-sudo yum remove docker \
-    docker-client \
-    docker-client-latest \
-    docker-common \
-    docker-latest \
-    docker-latest-logrotate \
-    docker-logrotate \
-    docker-engine
-
-curl -sSL https://get.daocloud.io/docker | sh
-
-
-sudo systemctl start docker  
-
-cat > /etc/docker/daemon.json <<EOF 
-{
-	"exec-opts": ["native.cgroupdriver=systemd"],	
-	"log-driver": "json-file",
-	"log-opts": {"max-size": "100m" },
-	"storage-driver": "overlay2", "storage-opts": ["overlay2.override_kernel_check=true" ]
-}
-EOF	
+# 设置 Docker 开机自启动
+sudo systemctl enable docker
+# 启动 Docker
+sudo systemctl start docker

aed7fb0f4423fb139665afdf224a3f8b00826efe
tom <121294318@qq.com>
2021-06-01 21:18:19 +0800

    Update k8s_master.sh

--- k8s_master.sh
+++ k8s_master.sh
@@ -9,7 +9,7 @@ apt update
 apt-get install -y kubelet=1.19.3-00 kubeadm=1.19.3-00 kubectl=1.19.3-00
 # show the version of kubenetes
 kubeadm version
-images=(kube-apiserver:v1.19.3 kube-controller-manager:v1.19.3 kube-scheduler:v1.19.3 kube-proxy:v1.19.3 pause:3.2 etcd:3.4.13-0 coredns:1.7.0)
+images=(kube-apiserver:v1.19.3 kube-controller-manager:v1.19.3 kube-scheduler:v1.19.3 kube-proxy:v1.19.3 pause:3.2 etcd:3.4.13-0 coredns:1.7.0) # 这行报错，使用: bash 脚本.sh
 echo $images
 for imageName in ${images[@]};do echo $imageName; done
 #for imageName in ${images[@]};do docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName; docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName; done
